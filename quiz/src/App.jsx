82b6ccd30a6b5b15f931fee311304c78845fb676
jatinssh <jatin7838066@gmail.com>
2024-01-27 21:57:18 +0530

    updated

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-
+import Quiz from './Components/Quiz/Quiz'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    
+   <Quiz/>
       
     </>
   )
---
+++ quiz/src/assets/data.js
@@ -0,0 +1,34 @@
+export const data =[
+    {
+    question: "Which consensus algorithm involves nodes in a network competing to solve complex mathematical problems?" ,
+    option1: "Proof-of-Authority ",
+    option2:" Proof-of-Work",
+    option3:"Delegated Proof-of-Stake",
+    option4:"Proof-of-Consensus",
+    answer: 2,
+    },
+    {
+        question:"What is the term for a permissioned blockchain where access is restricted to specific participants?",
+        option1:"Public",
+        option2:"Private",
+        option3:"Hybrid",
+        option4:"open",
+        answer: 2,
+    },
+    {
+        question:"What cryptographic technique is used to ensure the confidentiality of data on a blockchain?",
+        option1:"Decryption",
+        option2:"Hashing",
+        option3:"Encryption",
+        option4:"Tokenization",
+        answer:3,
+    },
+    {
+        question:"What is the term for the process of recording transactions in a chronological order on a blockchain?",
+        option1:"Timestamping",
+        option2:" Data Sequencing",
+        option3:"Transaction Logging",
+        option4:"Event Chronology",
+        answer:1,
+    },
+];
